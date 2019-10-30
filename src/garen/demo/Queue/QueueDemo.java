24d31f2b5c4cca1787f05fd50fdd985455c897b7
Garen <garen2994@hotmail.com>
2019-10-30 16:17:13 +0800

    :heavy-check-mark: Circular Queue update

--- src/garen/demo/Queue/QueueDemo.java
+++ src/garen/demo/Queue/QueueDemo.java
@@ -4,51 +4,63 @@ import garen.demo.Stack.LinkedlistStack;
 
 public class QueueDemo {
     public static void main(String[] args) {
-        SeqQueue<Integer> queue = new SeqQueue<Integer>(6);
-        queue.add(20);
-        queue.add(18);
-        queue.add(16);
-        queue.add(14);
-        queue.add(12);
-        queue.add(10);
-//        queue.add(8);
-        queue.remove();
-        System.out.println(queue.element());
-//        queue.add(6);
-        System.out.println(queue.toString());
+//        SeqQueue<Integer> queue = new SeqQueue<Integer>(6);
+//        queue.add(20);
+//        queue.add(18);
+//        queue.add(16);
+//        queue.add(14);
+//        queue.add(12);
+//        queue.add(10);
+////        queue.add(8);
+//        queue.remove();
+//        System.out.println(queue.element());
+////        queue.add(6);
+//        System.out.println(queue.toString());
+//
+//        System.out.println("============链式队列==============");
+//        LinkedListQueue<String> queue1 = new LinkedListQueue<>();
+//        queue1.add("Garen");
+//        queue1.add("Sun");
+//        queue1.add("Olivia");
+//        queue1.add("Bitch");
+//        queue1.add("Sheila");
+//        System.out.println(queue1.toString());
+//        queue1.remove();
+//        queue1.remove();
+//        queue1.remove();
+//
+//        System.out.println(queue1.toString());
+//        System.out.println(queue1.size());
+//        System.out.println(queue1.peek());
+//
+//        System.out.println(queue1.isEmpty());
+//
+//        System.out.println("============两个栈实现队列==============");
+//        StackQueue<Integer> queue3 = new StackQueue<>();
+//        queue3.add(1);
+//        queue3.add(2);
+//        queue3.add(3);
+//        queue3.add(4);
+//        queue3.add(5);
+//        System.out.println(queue3.toString());
+//        System.out.println("队头元素:" + queue3.peek().toString());
+//        queue3.remove();
+//        System.out.println(queue3.toString());
+//        System.out.println("队头元素:" + queue3.peek().toString());
+//        queue3.remove();
+//        System.out.println(queue3.toString());
+//        System.out.println("队头元素:" + queue3.peek().toString());
 
-        System.out.println("============链式队列==============");
-        LinkedListQueue<String> queue1 = new LinkedListQueue<>();
-        queue1.add("Garen");
-        queue1.add("Sun");
-        queue1.add("Olivia");
-        queue1.add("Bitch");
-        queue1.add("Sheila");
-        System.out.println(queue1.toString());
-        queue1.remove();
-        queue1.remove();
-        queue1.remove();
-
-        System.out.println(queue1.toString());
-        System.out.println(queue1.size());
-        System.out.println(queue1.peek());
-
-        System.out.println(queue1.isEmpty());
-
-        System.out.println("============两个栈实现队列==============");
-        StackQueue<Integer> queue3 = new StackQueue<>();
-        queue3.add(1);
-        queue3.add(2);
-        queue3.add(3);
-        queue3.add(4);
-        queue3.add(5);
-        System.out.println(queue3.toString());
-        System.out.println("队头元素:" + queue3.peek().toString());
-        queue3.remove();
-        System.out.println(queue3.toString());
-        System.out.println("队头元素:" + queue3.peek().toString());
-        queue3.remove();
-        System.out.println(queue3.toString());
-        System.out.println("队头元素:" + queue3.peek().toString());
+        System.out.println("==================循环队列================");
+        CircularQueue<String> queue4 = new CircularQueue<>(8);
+        queue4.push("Karl");
+        queue4.push("Rob");
+        queue4.push("Jade");
+        queue4.push("Simone");
+        queue4.push("Naomi");
+        queue4.push("Tommy");
+        queue4.pop();
+        System.out.println(queue4.size());
+        System.out.println(queue4.toString());
     }
 }
