921caf06940782f43ad6036a065214aa6664b13b
Garen_Hong <hjl52994@sina.com>
2020-01-09 00:40:19 +0800

    :heavy_check_mark: Graph updated

--- src/garen/demo/graph/Graph.java
+++ src/garen/demo/graph/Graph.java
@@ -9,21 +9,27 @@ import java.util.LinkedList;
  * @Date :  2020/1/4 14:24
  */
 public class Graph<E> {
-    private Vertex<E>[] vertexList;  //顶点
-    private int[][] mGraph;  //用矩阵表示边
+    public Vertex<E>[] vertexList;  //顶点
+    public int[][] mGraph;  //用矩阵表示边
     
     //用于图的遍历
-    private LinkedList<Integer> stack;     //栈 (存要访问的节点的索引)
-    private LinkedList<Integer> queue;     //队列 （存索引）
-    private int size;   //当前顶点
+    
+    public LinkedList<Integer> stack;  //栈 (存要访问的节点的索引)
+    public LinkedList<Integer> queue;  //队列 （存索引）
+    public int size;   //当前顶点数
     
     //用于最小生成树应用
-    private int[] distance;  //记录到起点的距离
-    private int[] path; //记录最短路径经过的顶点
-    private int[][] dist;    //dist[i][j]记录i到j的最短距离
-    private int[][] prev;    //prev[i][j]=k表示i到j的最短路径会经过顶点k
+    public int[] distance;  //记录到起点的距离
+    public int[] path; //记录最短路径经过的顶点
+    public int[][] dist;    //dist[i][j]记录i到j的最短距离
+    public int[][] prev;    //prev[i][j]=k表示i到j的最短路径会经过顶点k
 //     int[] parent; //并查集
     
+    /**
+     * @description 构造方法
+     * @param maxSize
+     * @return
+     */
     public Graph(int maxSize) {
         vertexList = new Vertex[maxSize];
         mGraph = new int[maxSize][maxSize];
@@ -38,9 +44,9 @@ public class Graph<E> {
     }
     
     /**
+     * @description 初始化为没有边的图
      * @param
      * @return void
-     * @description 初始化为没有边的图
      */
     public void init() {
         for (int i = 0; i < mGraph.length; i++) {
@@ -56,9 +62,9 @@ public class Graph<E> {
     }
     
     /**
+     * @description 添加边
      * @param value
      * @return boolean
-     * @description 添加边
      */
     public boolean addVertex(E value) {
         if (size == vertexList.length) {
@@ -69,11 +75,11 @@ public class Graph<E> {
     }
     
     /**
+     * @description 添加边
      * @param start  弧尾
      * @param end    弧头
      * @param weight 权重
      * @return boolean
-     * @description 添加边
      */
     public boolean addEdge(int start, int end, int weight) {
         if (start < 0 || start >= mGraph.length || end < 0 || end >= mGraph.length) {
@@ -85,15 +91,16 @@ public class Graph<E> {
     }
     
     /**
+     * @description 深度优先搜索
      * @param
      * @return String
-     * @description 深度优先搜索
      */
     public String dfs() {
         StringBuilder sb = new StringBuilder();
         sb.append(vertexList[0].value).append(" ");
         vertexList[0].isVisited = true;
-        stack.push(0);  //将第一个节点入栈
+        //将第一个节点入栈
+        stack.push(0);
         while (!stack.isEmpty()) {
             int v = getUnvisitedVertex(stack.peek());
             if (v == -1) {
@@ -104,22 +111,23 @@ public class Graph<E> {
                 stack.push(v);
             }
         }
-        for (Vertex vertex : vertexList) {
+        for (Vertex<E> vertex : vertexList) {
             vertex.isVisited = false;
         }
         return sb.toString().trim();
     }
     
     /**
+     * @description 广度优先搜索
      * @param
      * @return String
-     * @description 广度优先搜索
      */
     public String bfs() {
         StringBuilder sb = new StringBuilder();
         sb.append(vertexList[0].value).append(" ");
         vertexList[0].isVisited = true;
-        queue.add(0);   //将第一个顶点入队
+        //将第一个顶点入队
+        queue.add(0);
         while (!queue.isEmpty()) {
             int v = getUnvisitedVertex(queue.peek());
             if (v != -1) {
@@ -134,9 +142,9 @@ public class Graph<E> {
     }
     
     /**
+     * @description 找到与某一顶点邻接但未被访问的顶点
      * @param v 某一顶点
      * @return int
-     * @description 找到与某一顶点邻接但未被访问的顶点
      */
     public int getUnvisitedVertex(int v) {
         for (int i = 0; i < mGraph.length; i++) {
@@ -149,41 +157,70 @@ public class Graph<E> {
     }
     
     /**
+     * @description 寻找单源最短路径--Dijkstra算法
      * @param s 起始点
      * @return void
-     * @description 寻找单源最短路径--Dijkstra算法
      */
-    public int[] dijkstra(int s) {
-        if (s < 0 || s >= vertexList.length) {
-            throw new ArrayIndexOutOfBoundsException("超出顶点集范围");
-        }
-        boolean[] st = new boolean[vertexList.length];
-//        distance = new int[vertexList.length];
+    public void dijkstra(int s) {
+        //初始化距离和路径
         for (int i = 0; i < vertexList.length; i++) {
-            for (int j = i + 1; j < vertexList.length; j++) {
-                if (mGraph[i][j] == 0) {
-                    mGraph[i][j] = Integer.MAX_VALUE;
-                    mGraph[j][i] = Integer.MAX_VALUE;
-                }
-            }
+            distance[i] = Integer.MAX_VALUE; //到源点的距离设置为无穷大
+            path[i] = -1; //到源点的路径初始化为-1
         }
-        for (int i = 0; i < vertexList.length; i++) {
-            distance[i] = mGraph[s][i];
-        }
-        st[s] = true;
+        distance[s] = 0;
         //处理从源点到其余顶点的最短路径
         for (int i = 0; i < vertexList.length; i++) {
             int min = Integer.MAX_VALUE;
-            int index = -1;
+            int k = 0;
             for (int j = 0; j < vertexList.length; j++) {
-                if (!st[j]) {
-                    if (distance[j] < min) {
-                        index = j;
-                        min = distance[j];
-                    }
+                if (!vertexList[j].isVisited && distance[j] < min) {
+                    vertexList[j].isVisited = true;
+                    min = distance[j];
+                    k = j;   //记录最小路径的顶点符号
+                }
+            }
+            
+            //修正当前最短路径和前驱结点
+            for (int j = 0; j < vertexList.length; j++) {
+                //找顶点k的临界点j，并更新它的邻接点到起点的最短路径
+                int temp = (mGraph[k][j] == Integer.MAX_VALUE ? Integer.MAX_VALUE : (min + mGraph[k][j]));
+                if (!vertexList[j].isVisited && (temp < distance[j])) {
+                    distance[j] = temp;
+                    path[j] = k;
                 }
             }
         }
-        return path;
+        // 打印Dijkstra最短路径的结果
+        printDijkstra(s);
+        //恢复visited为false，方便下次访问
+        for (Vertex<E> vertex : vertexList) {
+            vertex.isVisited = false;
+        }
+    }
+    
+    /**
+     * @description 打印Dijkstra最短路径的结果
+     * @param s
+     * @return void
+     */
+    public void printDijkstra(int s) {
+        //利用栈后进先出的特性，将路径逆序
+        LinkedList<Integer> stack = new LinkedList<>();
+        System.out.printf("dijkstra(%c): \n", (char) vertexList[s].value);
+        for (int i = 0; i < vertexList.length; i++) {
+            System.out.printf("  shortest(%c, %c)=%d 路径为：", (Character) vertexList[s].value, (Character) vertexList[i].value, distance[i]);
+            //这里可以用一个栈来存储顶点，然后出栈就是顺序输出了，而不是反向输出
+            //打印路径
+            stack.push(i);  //终点
+            int temp = path[i];
+            while (temp != -1) {
+                stack.push(temp);
+                temp = path[temp];
+            }
+            while (!stack.isEmpty()) {
+                System.out.printf("%c-->", (Character) vertexList[stack.pop()].value);
+            }
+            System.out.println();
+        }
     }
 }
--- src/garen/demo/graph/GraphTest.java
+++ src/garen/demo/graph/GraphTest.java
@@ -10,9 +10,10 @@ import org.junit.jupiter.api.Test;
  */
 public class GraphTest {
     private Graph g = new Graph(7);
+    
     @SuppressWarnings("unchecked")
     @Test
-    public void graphTest(){
+    public void graphTest() {
         //添加顶点
         g.addVertex('A');
         g.addVertex('B');
@@ -31,13 +32,13 @@ public class GraphTest {
 //         /*F*/ {  16,   7,   6, INF,   2,   0,   9},
 //         /*G*/ {  14, INF, INF, INF,   8,   9,   0}};
         //添加边
-        g.addEdge(0,1,12);
+        g.addEdge(0, 1, 12);
         g.addEdge(0, 5, 16); //AF
         g.addEdge(0, 6, 14); //AG
-        g.addEdge(1, 2,10);   //BC
+        g.addEdge(1, 2, 10);   //BC
         g.addEdge(1, 5, 7);   //BF
-        g.addEdge(2, 3,3); //CD
-        g.addEdge(2, 4,5); //CE
+        g.addEdge(2, 3, 3); //CD
+        g.addEdge(2, 4, 5); //CE
         g.addEdge(2, 5, 6); //CF
         g.addEdge(3, 4, 4); //DE
         g.addEdge(4, 5, 2); //EF
@@ -49,5 +50,11 @@ public class GraphTest {
         System.out.println("=======广度优先搜索=======");
         System.out.println(g.bfs());
         System.out.println();
+        
+        g.dijkstra(3);
+        System.out.println("路径：");
+        for (int i = 0; i < g.vertexList.length; i++) {
+            System.out.print(g.path[i] + " ");
+        }
     }
 }
