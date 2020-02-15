e0a1bd2264bb1cd2ea87648870ce7585c210703e
Garen_Hong <hjl52994@sina.com>
2020-02-16 00:04:12 +0800

    :heavy_check_mark: Heap updated

--- src/garen/demo/heap/MinHeap.java
+++ src/garen/demo/heap/MinHeap.java
@@ -20,39 +20,42 @@ public class MinHeap {
         minHeap = new int[maxSize];
     }
     
+    /**
+     * @description 构造方法：初始化并调整为一个小根堆（堆化）
+     * @param arr
+     * @param maxSize
+     * @return
+     */
     public MinHeap(int[] arr, int maxSize) {
         minHeap = new int[Math.max(maxSize, arr.length)];
         System.arraycopy(arr, 0, minHeap, 0, arr.length);
         size = arr.length;
-        
-        int pos = (size - 2) / 2;  //最初调整位置：最后叶节点的父亲节点
-        while (pos >= 0) {  //依次调整每个分支节点
+        int pos = (size - 2) / 2;
+        while(pos >= 0){//自下向上逐步扩展为堆
             shiftDown(pos, size - 1);
             pos--;
         }
-        
     }
     
     /**
-     * @description 自下而上的调整为小根堆(如果一个节点比他的父节点小, 向上移动)--用于插入操作
      * @param start
      * @return void
+     * @description 自下而上的调整为小根堆(如果一个节点比他的父节点小, 向上移动)--用于插入操作
      */
     private void shiftUp(int start) {
         int j = start;
-        int i = (j - 1) / 2;  //起始调整位置：分支节点
+        int i = (j - 1) / 2;
         int temp = minHeap[j];
-        while (j > 0) { //迭代条件：子节点不为根
-            if (temp >= minHeap[i]) { //只需比较这个子女与父亲的关系即可
+        while (j > 0) {
+            if (minHeap[i] <= temp) {
                 break;
             } else {
-                minHeap[j] = minHeap[i];    //父节点小于子节点，交换；子节点上移
+                minHeap[j] = minHeap[i];
                 j = i;
                 i = (j - 1) / 2;
             }
         }
         minHeap[j] = temp;
-        
     }
     
     /**
@@ -61,24 +64,58 @@ public class MinHeap {
      * @description 自上向下调整为小根堆(如果一个节点比他的子节点大, 向下移动)--用于删除操作和建堆
      */
     private void shiftDown(int start, int end) {
-        int i = start;  //最后一个节点的父亲节点（0开始）
-        int j = 2 * start + 1;     //最后一个节点
+        int i = start;
+        int j = 2 * start + 1;
         int temp = minHeap[i];
-        while (j <= end) {    //迭代条件：子节点不能超出end范围
+        while (j <= end) {
             if (j < end) {
-                j = minHeap[j] > minHeap[j + 1] ? j + 1 : j;//选择较小的孩子
+                j = minHeap[j] < minHeap[j + 1] ? j : j + 1;
             }
             if (temp < minHeap[j]) {
                 break;
-            } else {    //交换父节点与较小的孩子节点
+            } else {
                 minHeap[i] = minHeap[j];
                 i = j;
-                j = 2 * j + 1;//该节点子树进行递归
+                j = 2 * i + 1;
             }
         }
         minHeap[i] = temp;
     }
-    public void printMinHeap(int i){
+    /**
+     * @description 插入元素到小根堆 末尾
+     * @param data
+     * @return void
+     */
+    public void insert(int data){
+        if(size == minHeap.length){
+            throw new RuntimeException("Heap Full");
+        }
+        if(size < minHeap.length){
+            minHeap[size] = data;
+            shiftUp(size);
+            size++;
+        }
+    }
+    /**
+     * @description 删除堆顶元素(即小根堆中的最小元素)
+     * @param
+     * @return int
+     */
+    public int remove(){
+        if(size == 0){
+            throw new NullPointerException("Heap Null");
+        }
+        int x = minHeap[0];
+        minHeap[0] = minHeap[--size];
+        shiftDown(0, size);
+        return x;
+    }
+    /**
+     * @param i
+     * @return void
+     * @description 以广义表形式递归输出堆
+     */
+    public void printMinHeap(int i) {
         if (size > i) {
             System.out.print(minHeap[i]);
             if (2 * i + 1 < size || 2 * i + 2 < size) {
@@ -90,6 +127,6 @@ public class MinHeap {
             }
         }
     }
-    
 }
+
 
--- src/garen/demo/heap/MinHeapTest.java
+++ src/garen/demo/heap/MinHeapTest.java
@@ -11,8 +11,18 @@ import org.junit.jupiter.api.Test;
 public class MinHeapTest {
     @Test
     public void test(){
-        int[] arr = {53, 17, 78, 9, 45, 65, 87, 23};
-        MinHeap heap = new MinHeap(arr,20);
+        int[] arr = {9,2,3,5,6,7,10,1};
+        MinHeap heap = new MinHeap(arr,9);
+        System.out.print("堆对应的广义表为：");
+        heap.printMinHeap(0);
+        System.out.println();
+        System.out.println("===============插入元素==============");
+        heap.insert(4);
+        System.out.print("堆对应的广义表为：");
+        heap.printMinHeap(0);
+        System.out.println();
+        System.out.println("===============删除元素==============");
+        System.out.println("删除的元素：" + heap.remove());
         System.out.print("堆对应的广义表为：");
         heap.printMinHeap(0);
     }
