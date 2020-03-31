0e9010827b158f01290f74203936bec56b830013
Garen_Hong <hjl52994@sina.com>
2020-04-01 00:17:31 +0800

    :heavy_check_mark: Sort updated(Heap Sort added)

--- src/garen/demo/sort/HeapSort.java
+++ src/garen/demo/sort/HeapSort.java
@@ -1,5 +1,7 @@
 package garen.demo.sort;
 
+import java.util.Arrays;
+
 /**
  * @Title : 堆排序 ***
  * @Author : Garen Hou
@@ -7,19 +9,59 @@ package garen.demo.sort;
  * @Date :  2020/3/28 0:32
  */
 public class HeapSort {
-    public void heapSort() {
-        //TODO
+    public void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i); //建堆
+        }
+        int size = arr.length;
+        swap(arr, 0, --size); //排序：把堆顶和最后元素交换，再heapify
+        while (size > 0) {
+            heapify(arr, 0, size);
+            swap(arr, 0, --size);
+        }
     }
     
-    private void heapify() {
-        //TODO
+    /**
+     * @param arr
+     * @param i
+     * @param size
+     * @return void
+     * @description 大根堆调整(从上到下)
+     */
+    private void heapify(int[] arr, int i, int size) {
+        int left = i * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[largest] > arr[i] ? largest : i;
+            if (largest == i) {
+                break; //后面不执行
+            }
+            swap(arr, largest, i);
+            i = largest;
+            left = i * 2 + 1;
+        }
     }
     
-    private void heapInsert() {
-        //TODO
+    /**
+     * @param arr
+     * @param i
+     * @return void
+     * @description 建大根堆(从下到上)
+     */
+    private void heapInsert(int[] arr, int i) {
+        while (arr[i] > arr[(i - 1) / 2]) {
+            swap(arr, i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
     }
     
     private void swap(int[] arr, int i, int j) {
-        //TODO
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+    
 }
--- src/garen/demo/sort/SortTest.java
+++ src/garen/demo/sort/SortTest.java
@@ -94,4 +94,22 @@ public class SortTest {
         }
         System.out.println(succeed ? "Nice!" : "Something Wrong!");
     }
+    @Test
+    public void HeapSortTest() {
+        HeapSort hs = new HeapSort();
+        int testTime = 50000;
+        boolean succeed = true;
+        MyComparator myComparator = new MyComparator();
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = myComparator.generateRandomArray(100, 100);
+            int[] arr2 = myComparator.copyArray(arr1);
+            hs.heapSort(arr1);
+            myComparator.comparator(arr2);
+            if (!myComparator.isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Something Wrong!");
+    }
 }
